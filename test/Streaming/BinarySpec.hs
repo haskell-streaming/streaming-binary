a171a0fbad9f4f2fa5827d769ecbe339529f620b
Mathieu Boespflug <m@tweag.io>
2017-05-28 19:04:40 +0200

    More complete tests.

--- test/Streaming/BinarySpec.hs
+++ test/Streaming/BinarySpec.hs
@@ -15,14 +15,22 @@ spec :: Spec
 spec = do
     let input n = Q.fromLazy $ runPut $ replicateM_ n $ put (42 :: Int)
     describe "decoded" $ do
+      it "succeeds on empty inputs" $ do
+        output <- void (decoded @Int (input 0)) & S.toList_
+        output `shouldBe` []
       it "decodes single integers" $ do
-        output <- void (decoded (input 1)) & S.toList_
-        output `shouldBe` [42 :: Int]
+        output <- void (decoded @Int (input 1)) & S.toList_
+        output `shouldBe` [42]
       it "decodes multiple integers" $ do
-        output <- void (decoded (input 10)) & S.toList_
-        output `shouldBe` (replicate 10 (42 :: Int))
+        output <- void (decoded @Int (input 10)) & S.toList_
+        output `shouldBe` (replicate 10 42)
       it "decodes multiple integers even when the input is incomplete" $ do
         n <- fromIntegral <$> Q.length_ (input 10)
         let input' = Q.take (n - 1) (input 10)
-        output <- void (decoded input') & S.toList_
-        output `shouldBe` (replicate 9 (42 :: Int))
+        output <- void (decoded @Int input') & S.toList_
+        output `shouldBe` (replicate 9 42)
+      it "leaves the right amount of leftover on incomplete input" $ do
+        n <- Q.length_ (input 10)
+        let input' = Q.take (fromIntegral (n - 1)) (input 10)
+        (leftover, _, _) <- S.effects (decoded @Int input')
+        Q.length_ leftover `shouldReturn` (n `div` 10) - 1
